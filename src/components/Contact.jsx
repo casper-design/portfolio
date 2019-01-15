727b31e2849099f14181bfccace918bab6143dda
Michael Casper <michaelcasper@Michaels-MacBook.local>
2019-01-15 00:03:46 -0500

    Third commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
-import { Helmet } from 'react-helmet';
 
 class Contact extends Component {
     render() {
         return (
             <Grid>
-                <Helmet>
-                        <title>Michael Casper</title>
-                </Helmet>
                 <h1>Let's Talk</h1>
                 <Row>
                     <Col xs={12} sm={6} className='project-wrapper'>
